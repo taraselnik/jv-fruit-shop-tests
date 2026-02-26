a94c5d976838911995c449fa4be53eaee52ca38d
Taras Pustovyi <31005050+taraselnik@users.noreply.github.com>
2026-02-27 00:02:39 +0100

    implement unit for OperationStrategyImpl

--- src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/impl/OperationStrategyImpl.java
@@ -11,16 +11,23 @@ public class OperationStrategyImpl implements OperationStrategy {
 
     public OperationStrategyImpl(
             Map<FruitTransaction.Operation, OperationHandler> operationHandlers) {
-        if (operationHandlers == null) {
-            throw new IllegalArgumentException("Operation handlers map can't be null");
+        if (operationHandlers == null || operationHandlers.isEmpty()) {
+            throw new IllegalArgumentException("Operation handlers map can't be null or empty");
         }
-        this.operationHandlers = Map.copyOf(operationHandlers);
+        try {
+            this.operationHandlers = Map.copyOf(operationHandlers);
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Operation handlers cannot contain null keys or values", e);
+        }
     }
 
     @Override
     public OperationHandler getOperationHandler(FruitTransaction.Operation operation) {
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation can't be null");
+        }
         if (operationHandlers == null) {
-            throw new IllegalArgumentException("Operation handlers map can't be null");
+            throw new RuntimeException("Operation handlers are empty");
         }
         OperationHandler handler = operationHandlers.get(operation);
         if (handler == null) {
---
+++ src/test/java/core/basesyntax/strategy/impl/OperationStrategyImplTest.java
@@ -0,0 +1,71 @@
+package core.basesyntax.strategy.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.model.transaction.FruitTransaction;
+import core.basesyntax.strategy.handler.OperationHandler;
+import core.basesyntax.strategy.handler.impl.BalanceOperation;
+import core.basesyntax.strategy.handler.impl.PurchaseOperation;
+import core.basesyntax.strategy.handler.impl.SupplyOperation;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OperationStrategyImplTest {
+
+    private OperationStrategyImpl operationStrategyImpl;
+    private Map<FruitTransaction.Operation, OperationHandler> operationHandlers;
+
+    @BeforeEach
+    void setUp() {
+        operationHandlers = new HashMap<>();
+        operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
+        operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
+
+        operationStrategyImpl = new OperationStrategyImpl(operationHandlers);
+    }
+
+    @AfterEach
+    void tearDown() {
+        operationStrategyImpl = null;
+    }
+
+    @Test
+    void operationStrategyImplTest_nullOperationHandlers_notOk() {
+        assertThrows(IllegalArgumentException.class, () -> new OperationStrategyImpl(null));
+    }
+
+    @Test
+    void operationStrategyImplTest_emptyOperationHandlers_notOk() {
+        Map<FruitTransaction.Operation, OperationHandler> operationHandlers = Map.of();
+        assertThrows(IllegalArgumentException.class,
+                () -> new OperationStrategyImpl(operationHandlers));
+    }
+
+    @Test
+    void getOperationHandler_validOperationHandler_Ok() {
+        OperationHandler operationBalanceHandler =
+                operationStrategyImpl.getOperationHandler(FruitTransaction.Operation.BALANCE);
+        assertEquals(BalanceOperation.class, operationBalanceHandler.getClass());
+    }
+
+    @Test
+    void getOperationHandler_immutable_Ok() {
+        OperationHandler operationBalanceHandlerInitial =
+                operationStrategyImpl.getOperationHandler(FruitTransaction.Operation.BALANCE);
+        operationHandlers.put(FruitTransaction.Operation.BALANCE, new SupplyOperation());
+        OperationHandler operationBalanceHandlerFinal =
+                operationStrategyImpl.getOperationHandler(FruitTransaction.Operation.BALANCE);
+        assertEquals(BalanceOperation.class, operationBalanceHandlerInitial.getClass());
+        assertEquals(BalanceOperation.class, operationBalanceHandlerFinal.getClass());
+    }
+
+    @Test
+    void getOperationHandler_emptyOperation_notOk() {
+        assertThrows(IllegalArgumentException.class,
+                () -> operationStrategyImpl.getOperationHandler(null));
+    }
+}
